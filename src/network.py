a25b5825805bc9901af73e29753f69291978bec2
Peter Mann <pm78@st-andrews.ac.uk>
2018-10-10 00:02:19 +0100

    Create network.py
    Uploaded python 2.7 file to generate a network using networkx and store the data to a txt file.

---
+++ src/network.py
@@ -0,0 +1,34 @@
+# (c) Peter Mann (2018) [University of St Andrews].
+#
+#	Generates a networkx graph and outputs the formatted data to a txt file
+# that can be read by the c++ program. This should be run in python 2.7.
+# The resulting file has the following format:
+# line number = node ID
+# numbers (space separated) = neighbours
+#
+# To-Do: use JSON rather than txt file. 
+
+
+import networkx 
+
+# create the network
+N = 1000                                # number of nodes in the network
+kmean = 5                               # mean degree of a node
+phi = (kmean + 0.0) / N                 # probability of two nodes being connected
+g = networkx.erdos_renyi_graph(N, phi)
+
+# clean the network
+g = g.subgraph(max(networkx.connected_components(g), key = len)).copy()
+g.remove_edges_from(list(g.selfloop_edges()))
+
+# output data to file
+output = open("data.txt", "w")
+for n in g.nodes():                     # for node in graph
+    e = g.neighbors(n)                  # list of neigbhours of node n
+    for k,v in enumerate(e):            # iterate over ns neighbours
+        v = str(v)                      # convert neighbour ID to string
+        output.write(v)                 # print string ID to file
+        if k != len(list(e))-1:         # if the element is not the final neighbour:
+            output.write(" ")           # separate data by single space
+    output.write("\n")                  # print newline for each node
+output.close()                          # close the output file
