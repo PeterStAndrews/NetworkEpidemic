d2ffc3e51da4caf81f0b7e37dda09be7bb7294a0
Peter Mann <pm78@st-andrews.ac.uk>
2018-10-11 20:32:43 +0100

    Update test_EVENT_DRIVEN.cpp

--- test/test_EVENT_DRIVEN.cpp
+++ test/test_EVENT_DRIVEN.cpp
@@ -29,32 +29,33 @@ SCENARIO( "Lets create an epidemic over a network", "[epidemic]") {
 
 	GIVEN( "A data.txt file containing network data ..." ){
 
-		GRAPH g; 								   	 // create graph class
-    	GRAPH::ReturnValue1 g_data = g.get_graph();  // call method `get_graph`
-    	GRAPH::NODES nodes = g_data.nodes;           // extract node list
+		GRAPH g; 			     	     // create graph class
+    		GRAPH::ReturnValue1 g_data = g.get_graph();  // call method `get_graph`
+    		GRAPH::NODES nodes = g_data.nodes;           // extract node list
 
-    	EVENT_DRIVEN sim(nodes);                     // create simulation object
-    	sim.copyGraph(nodes);                        // create network copy.
+    		EVENT_DRIVEN sim(nodes);                     // create simulation object
+    		sim.copyGraph(nodes);                        // create network copy.
+	
+    		WHEN("A disease with beta = 0.5 and gamma = 0.3 ... "){
 
-    	WHEN("A disease with beta = 0.5 and gamma = 0.3 ... "){
+    			std::map<std::string,double> params;      // create params dict
+    			params["beta"] = (double) 0.5; 		  // initialise the infection probability
+        		params["gamma"] = 0.3;			  // initialise the recovery probability
+        		sim._do(params); 			  // simulate the epidemic
 
-    		std::map<std::string,double> params;      // create params dict
-    		params["beta"] = (double) 0.5; 			  // initialise the infection probability
-        	params["gamma"] = 0.3;			 		  // initialise the recovery probability
-        	sim._do(params); 						  // simulate the epidemic
+        		std::map<std::string, double> res;	  // create results map
+        		res = sim.get_results();		  // extract simulation results
 
-        	std::map<std::string, double> res;		  // create results map
-        	res = sim.get_results();				  // extract simulation results
-
-        	THEN("expect at least one root node was infected or an epidemic occured ..."){
+        		THEN("expect at least one root node was infected or an epidemic occured ..."){
         		
-        		REQUIRE(res["S"] > 0);
-        		REQUIRE(res["I"] == 0);
-        		REQUIRE(res["R"] > 0);
-        		REQUIRE( res["S"] + res["I"] + res["R"] == g_data.nodes.size());
-        	}
-        }								 
-	}
+        			REQUIRE(res["S"] > 0);		  // non-zero susceptibles
+        			REQUIRE(res["I"] == 0);		  // no infected nodes 
+        			REQUIRE(res["R"] > 0);	          // at least one removed node
+								  // population is conserved
+        			REQUIRE( res["S"] + res["I"] + res["R"] == g_data.nodes.size());
+        		}
+        	}								 
+    	}
 }
 
 
