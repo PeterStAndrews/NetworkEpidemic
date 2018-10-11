5939ba15e5c324cd23d9200daf848035136da000
Peter Mann <pm78@st-andrews.ac.uk>
2018-10-11 20:42:27 +0100

    Update event_driven.hpp

--- src/event_driven.hpp
+++ src/event_driven.hpp
@@ -48,7 +48,9 @@ class EVENT_DRIVEN{
     //
     //      call_event: grabs the next event in the queue and calls it
     //
-    private:
+    //      get_results: returns the results map containing experiment data
+    //
+private:
     
     using EFFECT = void(EVENT_DRIVEN::*)(unsigned int);
     
@@ -76,16 +78,19 @@ class EVENT_DRIVEN{
     
     typedef std::vector<EVENT> EVENTS;  // type: list of events
     
-    public:
+public:
     double beta, gamma;                 // model parameters
     GRAPH::NODES nodes, _nodes;         // network nodes
     bool propensity;
     std::priority_queue < EVENT, EVENTS, COMPARISON > queue;
-    int I,R;
+    int S,I,R;
     unsigned int root,r2;               // root of infection tree
     double r1;                          // declare random doubles
     double tau_t, tau_r;                // declare time interval(s)
     double t;                           // start time
+    
+                                        // object to store results
+    std::map<std::string, double> results;
     
     EVENT_DRIVEN(GRAPH::NODES nodes) : nodes(nodes) {}
     
@@ -102,6 +107,8 @@ class EVENT_DRIVEN{
     void recover(unsigned int ID);
     
     void call_event();
+    
+    std::map<std::string, double> get_results();
 };
 
 
@@ -133,25 +140,50 @@ void EVENT_DRIVEN::setup(std::map<std::string,double> params){
     queue = std::priority_queue < EVENT, EVENTS, COMPARISON >();
     nodes = _nodes;                // something going one with this line, sometimes ...
     propensity = true;
-    R = 0;
+    
     t = 0.0;
     // select index(s) for root of infected tree (if multiple: pick unique)
     root = random_integer(1, static_cast<unsigned int>(nodes.size()));
     infect(nodes[root].ID);
+    
 }
 
 void EVENT_DRIVEN::teardown(){
     // type: function
-    // definition: reports the outcome of the experiment, parameters and metadata
+    // definition: reports the outcome of the experiment, parameters and
+    // and metadata for the run.
+    S = 0;
+    I = 0;
+    R = 0;
+    
     for(GRAPH::NODES::const_iterator it = nodes.begin(); it != nodes.end(); ++it){
+        if((*it).STATUS < 0){
+            ++S;
+        }
+        if((*it).STATUS == 0){
+            ++I;
+        }
         if((*it).STATUS > 0){
             ++R;
         }
     }
-    double epidemic = (double) R / (double) nodes.size();
-    std::cout << beta << " " << epidemic  << "\n";
+    // output to terminal
+    //double epidemic = (double) R / (double) nodes.size();
+    //std::cout << beta << " " << epidemic  << " a" << "\n";
+    
+    results["beta"] = beta;
+    results["gamma"] = gamma;
+    results["S"] = static_cast<double>(S);
+    results["I"] = static_cast<double>(I);
+    results["R"] = static_cast<double>(R);
+    
 }
 
+std::map<std::string, double> EVENT_DRIVEN::get_results(){
+    // type: function
+    // definition: returns the results map containing experiment data.
+    return results;
+}
 
 void EVENT_DRIVEN::infect(unsigned int ID){
     // type: function
@@ -218,7 +250,6 @@ void EVENT_DRIVEN::call_event(){
         
         // play it out
         (this->*e.EVENT_DRIVEN::EVENT::effect)(e.location);
-        
         
         // pop the event
         queue.pop();
